54aa468d74c5c6a34689c1c613a2c35dd63cff89
Manit Ghogar <manitghogar@gmail.com>
2016-02-19 12:21:07 -0500

    LEARN MORE PRINTING
    Learnt more about format variables and joining variables together into
    strings

---
+++ 7/ex7.py
@@ -0,0 +1,28 @@
+#prints a string -sentence
+print "Mary had a little lamb."
+#prints a string by implementing a string into an embedded format character.
+print "Its fleece was white as %s." % 'snow'
+#prints a string - sentence
+print "And everywhere that Mary went."
+#prints ten periods together like this ...........
+print "." * 10 # what'd that do?
+#assigns values to variables end1 to end12
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+
+#prints the values of teh above variables. The comma allows 
+#the output to be displayed as one line when the function is run
+print end1 + end2 + end3 + end4 + end5 + end6,
+print end7 + end8 + end9 + end10 + end11 + end12
+
