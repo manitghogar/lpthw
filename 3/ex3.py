22251708b28e7271f2fb19170ab448867a7a5df5
Manit Ghogar <manitghogar@gmail.com>
2016-02-18 19:19:23 -0500

    Study Drill 1
    Writing comments above each function

---
+++ 3/ex3.py
@@ -0,0 +1,29 @@
+#start of the task, will start counting chickens
+print "I will now count my chickens:"
+#counts number of hens
+print "Hens", 25 + 30 / 6
+#counts number of Roosters
+print "Roosters", 100 - 25 * 3 % 4
+#will start counting eggs
+print "Now I will count the eggs:"
+#counting eggs
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+#question - boolean
+print "Is it true that 3 + 2 < 5 - 7?"
+#answer to the boolean questions
+print 3 + 2 < 5 - 7
+#clarifying first question - answers with 5
+print "What is 3 + 2?" , 3 + 2
+#clarifying first question - answers with -2
+print "What is 5 - 7?" , 5 - 7
+#realizes why the first question was false
+print "Oh, that's why it's False."
+#asks for more
+print "How about some more."
+#question 2 - boolean
+print "Is it greater?" , 5 > -2
+#proof to question 2 - answers with True
+print "Is it greater or equal?" , 5>= -2
+#proof to question 2 - answers with False
+print "Is it less or equal?" , 5 <= -2
+
