eac70002a79e020cb6bb54a37adc77290cc73592
Manit Ghogar <manitghogar@gmail.com>
2016-02-20 14:16:18 -0500

    Print variables into strings
    Learn about printing variables into strings,and also about commenting
    out multiple lines.

---
+++ 9/ex9.py
@@ -0,0 +1,18 @@
+# Here's some new strange stuff, remember type it exactly.
+
+#setting values to the variable 'days'
+days = "Mon Tue Wed Thu Fri Sat Sun"
+#setting values for the variable 'months'
+months = "Jan\nFeb\nMar\nApr\May\nJun\nJul\nAug"
+#printing the variable days after a string
+print "Here are the days: ", days
+#printing the variable months after a string
+print "Here are the months: ", months
+#learning about commenting out multiple lines out at once
+print """
+There's somethign going on here.
+With the three double-quotes. 
+We'll be able to type as much as we like. 
+Even 4 lines if we want, or 5, or 6.
+"""
+
