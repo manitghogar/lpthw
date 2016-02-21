2f733c1c947fde33cf6a2fefb7b766d143d0a9ff
Manit Ghogar <manitghogar@gmail.com>
2016-02-21 12:24:49 -0500

    Prompting People
    Learnt to prompt people by asking questions in a more effective way.
    The prompt is incorporated into the question, and the value of the
    answer is assigned to the variable.

---
+++ 12/ex12.py
@@ -0,0 +1,25 @@
+#puts the prompt in the raw_input argument and assigns the answer to the value of variable.
+age =  raw_input ("How old are you? ")
+height = raw_input ("How tall are you? ")
+weight = raw_input ("How much do you weigh? ")
+#prints a string with format variables that call on the values of age, height, weight
+print "So, you're %r old, %r tall and %r heavy." % (age, height, weight)
+
+
+"""
+Pydoc is a document generator and online help system. It automatically
+generates documentation from Python modules. It tells you how functions work
+and what to input in them.
+
+Pydoc raw_input tells you what to put in the raw_input argument
+
+Pydoc Open - allows you to open a file using the file() type, 
+returns a file object.
+
+Pydoc file - opens a file after specifying the mode (reading, writing or appending). 
+The file will be created if it doesn't exist.
+
+Pydoc os - provides help on module os - provides documentation on where python sits in 
+the operating system
+
+Pydoc sys - (not too sure about this one)
