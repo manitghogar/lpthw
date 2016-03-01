9b322f75b8c10cba7e77c3085b05437e52eea780
Manit Ghogar <manitghogar@gmail.com>
2016-02-29 19:15:32 -0500

    Access elements inside of a list
    Learnt about index numbers and how to pull items inside of a list

---
+++ 34/ex34.py
@@ -0,0 +1,58 @@
+animals = ['bear','python','peacock','kangaroo','whale','platypus']
+
+#1 The animal at 1 = 'python'
+#The second (2nd) animal is at 1 and is a python
+#The animal at 1 is the 2nd animal and is a python
+print animals[1]
+#2 The third (3rd) animal = 'peacock'
+#The third animal is at 2 and is a peacock
+#The animal at 2 is the third animal and is a peacock
+print animals[2]
+#The first (1st) animal = 'bear'
+#The first animal is at 0 and is a bear
+#The animal at 0 is the first animal and is a bear
+print animals[0]
+#The animal at 3 = 'kangaroo'
+#The fourth animal is at 3 and is a kangaroo
+#The animal at 3 is the fourth animal and is a kangaroo
+print animals[3]
+#The fifth (5th) animal = 'whale'
+#The fifth animal is at 4 and is a whale
+#The animal at 4 is the fifth animal and is a whale
+print animals[4]
+#The animal at 2 = 'peacock'
+#The third animal is at 2 and is a peacock
+#The animal at 2 is the third animal and is a peacock
+print animals[2]
+#The sixth (6th) animal = 'platypus'
+#The sixth animal is at 5 and is a platypus
+#The animal at 5 is the sixth animal and is a platypus
+print animals[5]
+#The animal at 4 = 'whale'
+#The fifth animal is at 4 and is a whale
+#The animal at four is the fifth animal and is a whale
+print animals[4]
+
+#2010 in January 1, 2010 is an ordinal number as you can not have 2010 without 2009.
+#It is not independant of other numbers in the list.
+
+sports = ['soccer', 'basketball', 'football', 'baseball']
+
+#1. Return Soccer
+# Soccer is the first sport and is at 0
+print sports[0]
+
+#2. Return football
+# Football is the third sport and is at 2
+print sports[2]
+
+#3. Return basketball
+# Basketball is the second sport and is at 1
+print sports[1]
+
+#4. Return baseball
+# baseball is the fourth sport and is at 3
+print sports[3]
+
+
+
